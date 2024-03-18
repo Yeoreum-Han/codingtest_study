22f3860106c56b2e43f98e59ddcc46b0dfde84dc
Yeoreum Han <yrh.summer@gmail.com>
2024-03-18 16:36:56 +0900

    입력과계산_a*b

---
+++ BOJ/sprout/10998.js
@@ -0,0 +1,5 @@
+/*두 정수 A와 B를 입력받은 다음, A*B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10) */
+
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+console.log(parseInt(input[0])*parseInt(input[1]));
