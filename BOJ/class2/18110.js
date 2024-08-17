e4fb22de3fcc8c8881e2ffd0930d67f7e7d2f5f7
Yeoreum Han <yrh.summer@gmail.com>
2024-08-17 16:04:29 +0900

    solved.ac-입력 주의

---
+++ BOJ/class2/18110.js
@@ -0,0 +1,18 @@
+// solved.ac - 입력 주의
+
+const input = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n').map(Number);
+const N = input.shift();
+if (N == 0) {
+	console.log(0);
+	return;
+}
+const limit = Math.round(N * 0.15);
+console.log(
+	Math.round(
+		input
+			.sort((a, b) => a - b)
+			.slice(limit, N - limit)
+			.reduce((r, v) => r + v, 0) /
+			(N - 2 * limit)
+	)
+);
